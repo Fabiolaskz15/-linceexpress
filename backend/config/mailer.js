a255caebc9b8aca4438d4a071fed69164e1151f9
vanee <PC@PC30.PanesCalientes.com.mx>
2026-06-18 10:01:30 -0600

    Cambiar SMTP Gmail a puerto seguro

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -5,20 +5,22 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    requireTLS: true,
+    port: 465,
+    secure: true,
     family: 4,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-    connectionTimeout: 20000,
-    greetingTimeout: 20000,
-    socketTimeout: 20000
+    connectionTimeout: 60000,
+    greetingTimeout: 60000,
+    socketTimeout: 60000,
+    tls: {
+        rejectUnauthorized: false
+    }
 });
 
-transporter.verify((error, success) => {
+transporter.verify((error) => {
 
     if (error) {
 
